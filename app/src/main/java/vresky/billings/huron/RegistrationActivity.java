70d89aa362145dc1da4ee57b43d51fdba43281fd
BTzen <matthew_fb@live.ca>
2017-01-05 17:03:07 -0500

    Push up-to-date version for project meeting

---
+++ app/src/main/java/vresky/billings/huron/RegistrationActivity.java
@@ -0,0 +1,17 @@
+package vresky.billings.huron;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Matt on 05/01/2017.
+ */
+public class RegistrationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+}
